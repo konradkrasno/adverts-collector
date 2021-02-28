e14a4883f0f408717a72095525b4908485b44c31
konradkrasno <konradkrasno@gmail.com>
2021-02-28 01:06:50 +0100

    fix staticfiles dirs settings

--- adverts_collector/settings.py
+++ adverts_collector/settings.py
@@ -135,10 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR.joinpath("staticfiles")
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
+STATICFILES_DIRS = (BASE_DIR.joinpath("staticfiles"),)
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 

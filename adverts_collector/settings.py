08820a370eed4b03adc38d68959b124137293004
konradkrasno <konradkrasno@gmail.com>
2021-02-28 01:10:35 +0100

    delete STATICFILES_DIRS

--- adverts_collector/settings.py
+++ adverts_collector/settings.py
@@ -138,8 +138,6 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR.joinpath("staticfiles")
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (BASE_DIR.joinpath("staticfiles"),)
-
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
